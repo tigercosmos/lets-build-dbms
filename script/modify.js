000dee1a83b97d9538e7e25647f38e8737ba89da
tigercosmos <phy.tiger@gmail.com>
2018-10-15 10:57:59 +0800

    add GA

--- script/modify.js
+++ script/modify.js
@@ -13,7 +13,7 @@ dirPaths.forEach(dirPath => {
         let data = fs.readFileSync(filePath, 'utf-8');
         data = data.replace("Published with GitBook", '');
         tmp = data.split("</head>");
-        data = tmp[0] + `` +
+        data = tmp[0] + `<script async src="https://www.googletagmanager.com/gtag/js?id=UA-84804118-5"></script><script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-84804118-5');</script>` +
             tmp[1];
         fs.writeFileSync(filePath, data, 'utf-8');
         console.log(file, "has been modified.");
